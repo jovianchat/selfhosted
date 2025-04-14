48794110972a876a3943e8fe7bbfc9012b8ee4e7
jsbhullar42 <dev.h8671@slmails.com>
2025-04-14 13:25:53 -0700

    removed nginx AND seperated artifacts build from staging and production GHA

--- ci/docker_entrypoint.sh
+++ ci/docker_entrypoint.sh
@@ -2,6 +2,7 @@
 # Entrypoint script for Container
 
 # Change ownership of Working Directory to UID 1000
+# Needed as .data is mount point of volume and can have different UID
 echo "Changing ownership of /app/.data to UID 1000..." >&2
 chown -R 1000:1000 /app/.data
 
